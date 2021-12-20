41d994dee7606d0f148f481869fa85070dce1862
vitruvianarendt <hanahh2001@gmail.com>
2021-12-20 01:34:42 +0100

    worked on changepassword service and component/page

--- Homework3/Frontend/parking-app/src/app/pages/auth/changepassword/changepassword.component.ts
+++ Homework3/Frontend/parking-app/src/app/pages/auth/changepassword/changepassword.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Changepassword } from 'src/app/core/models/changepassword';
+import { ChangepasswordService } from 'src/app/core/services/changepassword.service';
 
 @Component({
   selector: 'app-changepassword',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangepasswordComponent implements OnInit {
 
-  constructor() { }
+ 
+  changePasswordForm = new FormGroup({
+    oldpassword: new FormControl(),
+    newpassword: new FormControl(),
+    confirmnewpassword: new FormControl()
+});  
 
-  ngOnInit(): void {
+  isSubmitted = false;
+  constructor(public formBuilder: FormBuilder, private changePasswordService: ChangepasswordService, private router: Router) { }
+  ngOnInit() {
+    this.changePasswordForm = this.formBuilder.group({
+      oldpassword: ['', [Validators.required]],
+      newpassword: ['', [Validators.required, Validators.pattern(/^(?=.*[A-z])(?=.*[0-9])\S{6,128}$/)]],
+      confirmnewpassword: ['', [Validators.required]],
+
+    })
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.changePasswordForm.valid) {
+      console.log('Please provide all the required values!');
+      return false;
+    } else {
+      let data = new Changepassword().deserialize({ old_password: this.changePasswordForm.controls['oldpassword'].value, new_password: this.changePasswordForm.controls['newpassword'].value });
+      let token = localStorage.getItem('token');
+      this.changePasswordService.changePassword(data, token).subscribe((res) => {
+        if (res.status == false) {
+          console.log("oops");
+        } else if (res.status === true) {
+          localStorage.removeItem('token');
+          console.log("successful");
+          this.router.navigate(['auth/login']);
+        } 
+      });
+      this.changePasswordForm.reset();
+    }
+    return;
   }
 
 }
