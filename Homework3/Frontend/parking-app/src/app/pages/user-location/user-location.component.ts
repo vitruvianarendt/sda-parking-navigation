5892ad7d2ae51d9902683c3f13de676b18e7e320
terezag07 <tereza.grujevska@outlook.com>
2022-01-31 16:14:07 +0100

    UserLocation

--- Homework3/Frontend/parking-app/src/app/pages/user-location/user-location.component.ts
+++ Homework3/Frontend/parking-app/src/app/pages/user-location/user-location.component.ts
@@ -1,39 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { timeout } from 'rxjs';
 declare const L: any;
+
+
 @Component({
   selector: 'app-user-location',
   templateUrl: './user-location.component.html',
   styleUrls: ['./user-location.component.scss']
 })
+
+
 export class UserLocationComponent implements OnInit {
 title='UserLocation';
 
 
 ngOnInit() {
   if(!navigator.geolocation){
-    console.log('locayion is not supported');
+    console.log('location is not supported');
   }
-navigator.geolocation.getCurrentPosition((position) => {
+
+  navigator.geolocation.getCurrentPosition((position) => {
   const coords = position.coords;
 
-  console.log(`lat: ${position.coords.latitude}, lon:${position.coords.longitude} `
-  );
+  console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude} `);
+ 
   let map = L.map('map').setView([coords.latitude, coords.longitude], 13);
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGVyZXphMDciLCJhIjoiY2t4aXlkcTB2NHdibDJvbzFycHNqZjAyYSJ9.irVCUJrRQDaMxMGHnq1BCg', {
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGVyZXphMDciLCJhIjoiY2t4aXlkcTB2NHdibDJvbzFycHNqZjAyYSJ9.irVCUJrRQDaMxMGHnq1BCg',
+   {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(map);
-    let marker = L.marker([coords.latitude, coords.longitude]).addTo(map);
-    marker.bindPopup('<b>ME</b>').openPopup();
+    accessToken: 'pk.eyJ1IjoidGVyZXphMDciLCJhIjoiY2t4aXlkcTB2NHdibDJvbzFycHNqZjAyYSJ9.irVCUJrRQDaMxMGHnq1BCg'
+  }).addTo(map);
+   
+  let marker1 = L.marker([coords.latitude, coords.longitude]).addTo(map);
+    marker1.bindPopup('<b>ME</b>').openPopup();
 
-    let marker1 = L.marker([coords.latitude+0.0005, coords.longitude+0.0007]).addTo(map);
-    marker.bindPopup('<b>FREE PARKING</b>').openPopup();
+  let marker2 = L.marker([coords.latitude+0.002, coords.longitude+0.0007]).addTo(map);
+    marker2.bindPopup('<b>FREE PARKING</b>').openPopup();
 
 
 });  
@@ -47,9 +54,9 @@ watchPosition() {
   let id=navigator.geolocation.watchPosition(
     (position) => {
     console.log(
-      `lat: ${position.coords.latitude}, lon:${position.coords.longitude} `
+      `lat: ${position.coords.latitude}, lon: ${position.coords.longitude} `
     );
-    if(position.coords.latitude==desLat){
+    if(position.coords.latitude == desLat){
       navigator.geolocation.clearWatch(id);
     }
 
